f8825a760979821ae159d55214df1898c2e63fe2
Jin Hong Chin <jinhong.chin@afusion.com>
2016-12-22 11:41:14 +0800

    added better demo page

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -4,17 +4,18 @@ import * as platform from 'platform';
 import { Demo } from "./main-view-model";
 import { View } from "ui/core/view";
 import { ArchitectView } from 'nativescript-wikitudearchitectview'
+import * as frameModule from 'ui/frame'
 
 function pageLoaded(args) {
   var page = <View>args.object;
   page.bindingContext = new Demo();
   var architectView = page.getViewById<any>("architectView");
-  console.log('loaded');
+  console.log('pageLoaded');
 }
 exports.pageLoaded = pageLoaded;
 
-export function tap(eventData){
-  console.log('tapped')
+export function goToTestPage(eventData){
+  frameModule.topmost().navigate('test-page')
 };
 
 export function urlLoaded(eventData){
---
+++ demo/app/test-page.ts
@@ -0,0 +1,8 @@
+import * as frameModule from 'ui/frame'
+
+export function pageLoaded(eventData) {
+}
+
+export function goBack(eventData) {
+    frameModule.goBack();
+}
